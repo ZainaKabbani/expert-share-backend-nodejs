56f90159f226e40fd7772c26e0d825d18ebec885
ZainaKabbani <zainakabbani07@gmail.com>
2023-11-07 15:39:56 +0200

    Initial commit

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './vars/config.env' });
 
-// mongoose.connect('mongodb://127.0.0.1:27017/expert_share');
-// mongoose.connection.on('connected', () => console.log('Connected'));
-// mongoose.connection.on('error', () => console.log('Connection failed with - ',err));
+mongoose.connect('mongodb://127.0.0.1:27017/expert_share');
+mongoose.connection.on('connected', () => console.log('Connected'));
+mongoose.connection.on('error', () => console.log('Connection failed with - ',err));
 
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-mongoose.connect(DB).then(()=> console.log('connected successfully'))
+// mongoose.connect(DB).then(()=> console.log('connected successfully'))
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
